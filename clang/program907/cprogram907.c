431e73fad35948e9673cf5852ce0fbef1c9f6fb1
chaforinli <457535344@qq.com>
2023-05-08 17:15:39 +0800

    Add files via upload

---
+++ clang/program907/cprogram907.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+long eB77V718 = 1667739;
+short TAGaBiK = 822;
+int GYm9015 = 6275;
+void g65wq4D()
+{
+  //if (((((eB77V718 / (((((eB77V718 == 0) ? 1 : (eB77V718)) == 0) ? 1 : (((eB77V718 == 0) ? 1 : (eB77V718))))))) / (((((eB77V718 == 0) ? 1 : (eB77V718)) == 0) ? 1 : (((eB77V718 == 0) ? 1 : (eB77V718))))))) == (7460369))
+  // if(0)
+  // {
+  //   if (0)
+  //   {
+  //     long ZqngypUf = 9089644;
+  //     TAGaBiK ^= 770;
+  //     long eMLfWllC = 9587274;
+  //   }
+  //   if (((GYm9015 % (((((GYm9015 == 0) ? 1 : (GYm9015)) == 0) ? 1 : (((GYm9015 == 0) ? 1 : (GYm9015))))))) != ((TAGaBiK % (((((TAGaBiK == 0) ? 1 : (TAGaBiK)) == 0) ? 1 : (((TAGaBiK == 0) ? 1 : (TAGaBiK))))))))
+  //   // if(0)
+  //   {
+  //     long ouyddXV = 2246432;
+  //     TAGaBiK >>= 803;
+  //     long wuMcskr = 9485627;
+  //   }
+  // }
+  // else
+  // {
+     int B3903G66count = 0;
+    do
+    {
+       B3903G66count = B3903G66count + 1;
+      if (B3903G66count > 2)
+      {
+        break;
+      }
+      // TAGaBiK >>= 407;
+      // TAGaBiK ^= 984;
+    } while (1);
+    int T18dUW7Dcount = 0;
+    do
+    {
+      T18dUW7Dcount = T18dUW7Dcount + 1;
+      if (T18dUW7Dcount > 2)
+      {
+        break;
+      }
+      TAGaBiK >>= 33; /*if RHS >= 32 it's a infinite loop in clang-15 -O1*/
+      // TAGaBiK >>= 509;
+    } /*while(GYm9015);*/while ((TAGaBiK) >= (TAGaBiK++));
+  // }
+  // long hQfO8AXF = ((((3437301) % (((((eB77V718 == 0) ? 1 : (eB77V718)) == 0) ? 1 : (((eB77V718 == 0) ? 1 : (eB77V718))))))) + ((eB77V718 - (4048186))));
+  return;
+ }
+long JU5IfLU = 2163839;
+int main()
+{
+  g65wq4D();
+  printf("%d\n", eB77V718);
+  printf("%d\n", TAGaBiK);
+  printf("%d\n", GYm9015);
+  printf("%d\n", JU5IfLU);
+  return 0;
+}
