1d9764484dc25d7a22ccc5e0223b501558ab9573
chaforinli <457535344@qq.com>
2023-05-08 17:17:44 +0800

    Update cprogram907.c

--- clang/program907/cprogram907.c
+++ clang/program907/cprogram907.c
@@ -4,25 +4,6 @@ short TAGaBiK = 822;
 int GYm9015 = 6275;
 void g65wq4D()
 {
-  //if (((((eB77V718 / (((((eB77V718 == 0) ? 1 : (eB77V718)) == 0) ? 1 : (((eB77V718 == 0) ? 1 : (eB77V718))))))) / (((((eB77V718 == 0) ? 1 : (eB77V718)) == 0) ? 1 : (((eB77V718 == 0) ? 1 : (eB77V718))))))) == (7460369))
-  // if(0)
-  // {
-  //   if (0)
-  //   {
-  //     long ZqngypUf = 9089644;
-  //     TAGaBiK ^= 770;
-  //     long eMLfWllC = 9587274;
-  //   }
-  //   if (((GYm9015 % (((((GYm9015 == 0) ? 1 : (GYm9015)) == 0) ? 1 : (((GYm9015 == 0) ? 1 : (GYm9015))))))) != ((TAGaBiK % (((((TAGaBiK == 0) ? 1 : (TAGaBiK)) == 0) ? 1 : (((TAGaBiK == 0) ? 1 : (TAGaBiK))))))))
-  //   // if(0)
-  //   {
-  //     long ouyddXV = 2246432;
-  //     TAGaBiK >>= 803;
-  //     long wuMcskr = 9485627;
-  //   }
-  // }
-  // else
-  // {
      int B3903G66count = 0;
     do
     {
@@ -31,8 +12,6 @@ void g65wq4D()
       {
         break;
       }
-      // TAGaBiK >>= 407;
-      // TAGaBiK ^= 984;
     } while (1);
     int T18dUW7Dcount = 0;
     do
@@ -43,10 +22,7 @@ void g65wq4D()
         break;
       }
       TAGaBiK >>= 33; /*if RHS >= 32 it's a infinite loop in clang-15 -O1*/
-      // TAGaBiK >>= 509;
-    } /*while(GYm9015);*/while ((TAGaBiK) >= (TAGaBiK++));
-  // }
-  // long hQfO8AXF = ((((3437301) % (((((eB77V718 == 0) ? 1 : (eB77V718)) == 0) ? 1 : (((eB77V718 == 0) ? 1 : (eB77V718))))))) + ((eB77V718 - (4048186))));
+    } while ((TAGaBiK) >= (TAGaBiK++));
   return;
  }
 long JU5IfLU = 2163839;
